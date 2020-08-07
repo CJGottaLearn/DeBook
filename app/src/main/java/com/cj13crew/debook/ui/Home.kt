baf1bd72bed9a45532fadc36ba33aeed8d4ed068
Chiedu Egbumokei JR <chiedu75@gmail.com>
2020-08-08 01:26:03 +0300

    Initial commit

---
+++ app/src/main/java/com/cj13crew/debook/ui/Home.kt
@@ -0,0 +1,12 @@
+package com.cj13crew.debook.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cj13crew.debook.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
